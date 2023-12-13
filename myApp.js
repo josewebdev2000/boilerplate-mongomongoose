73656164e4cbb2dfedab19134ac1aafbf40c4744
jjbrach4 <jjbrach4@ValeUbu.myguest.virtualbox.org>
2023-12-13 17:52:18 -0400

    Set up mongoose and establish DB connection

--- myApp.js
+++ myApp.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
+const mongoose = require("mongoose");
 
+// Connect to the DB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 let Person;
 
