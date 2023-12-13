4d6af8679d183dc46708828dbe2d4f0d0f058e22
josewebdev2000 <josewebdev@proton.me>
2023-12-13 18:09:11 -0400

    Created Person Mongo Schema

--- myApp.js
+++ myApp.js
@@ -7,7 +7,20 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true
 });
 
-let Person;
+let Person = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  favoriteFoods: {
+    type: [String]
+  }
+});
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
